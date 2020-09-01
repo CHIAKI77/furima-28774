cbbcd3d0b8022894d41f5520530983483a39a6b2
CHIAKI77 <69070285+CHIAKI77@users.noreply.github.com>
2020-09-01 18:43:41 +0900

    userモデルの単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   
   # ユーザー新規登録時のバリデーション
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
+  # validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i}
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, presence: true, length: { minimum: 1 }, format: { with: PASSWORD_REGEX}
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX}
   validates :family_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
