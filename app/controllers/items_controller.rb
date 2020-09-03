7b355b798c47b0fe5aaa789de053caa18b52f994
CHIAKI77 <69070285+CHIAKI77@users.noreply.github.com>
2020-09-03 15:59:23 +0900

    商品出品画面表示と未ログイン時のログイン画面への遷移実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
 
-  praivate
+  def new
+    @item = Item.new
+  end
+
+  private
 
   def item_params
     params.require(:item).permit(:image).merge(user_id: current_user.id)
