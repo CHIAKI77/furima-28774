d3ede62bff087db72342769d6b3829613504128e
CHIAKI77 <69070285+CHIAKI77@users.noreply.github.com>
2020-09-05 14:53:09 +0900

    itemモデルテストコードの実装

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,27 +4,19 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
-function price(){
-  // 画面が読み込まれた時に動くように定義する
-   const priceInput = document.getElementById("item-price");
-   const add_tax = document.getElementById("add-tax-price");//販売手数料
-   const profit = document.getElementById("profit");//販売利益
-   // 価格入力時に手数料、利益計算
-     priceInput.addEventListener('keyup', () => {
-         const value = priceInput.value; //value（入力の金額を定義）
-         
-       if (value >= 300 && value <= 9999999){
-         let fee = value * 0.1
-         let gains = value - fee
-         add_tax.textContent = fee;
-         profit.textContent = gains;
-     } else {
-       let fee = '-';
-       let gains = '-';
-       add_tax.textContent = fee;
-       profit.textContent = gains;
-     }
-   });
-  
+window.addEventListener("DOMContentLoaded", () => {
+  const path = location.pathname
+  const pathRegex = /^(?=.*item)(?=.*edit)/
+  if (path === "/items/new" || path === "/items" || pathRegex.test(path)) {
+    //    出品ページの場合　||　出品ページの検証にかかった場合 || 商品編集の場合
+    const priceInput = document.getElementById("item-price");
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+
+    priceInput.addEventListener("input", () => {
+      const inputValue = document.getElementById("item-price").value;
+        addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
+        profitDom.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
+    })
   }
-  window.addEventListener('load', price);
+});
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :item do
+    name { 'テスト' }
+    description { 'テスト' }
+    category_id { 2 }
+    condition_id { 2 }
+    shipping_burden_id { 2 }
+    shipping_region_id { 2 }
+    shipping_day_id { 2 }
+    price { '10000' }
+    user
   end
 end
