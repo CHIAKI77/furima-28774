7f04da0e3fd3011b96c6b012e3b3507d5f538387
CHIAKI77 <69070285+CHIAKI77@users.noreply.github.com>
2020-09-05 17:00:53 +0900

    itemモデルテストコード実装修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
+  before_action :move_to_index, except: [:index]
 
   def index
     @item = Item.all
@@ -22,5 +23,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_burden_id, :shipping_region_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
