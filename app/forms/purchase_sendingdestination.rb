e21a9b5c5d41524abadf8e7e909e814ceb3a3fd0
CHIAKI77 <69070285+CHIAKI77@users.noreply.github.com>
2020-09-12 21:28:51 +0900

    ダミー

--- app/forms/purchase_sendingdestination.rb
+++ app/forms/purchase_sendingdestination.rb
@@ -15,3 +15,4 @@ class PurchaseSendingdestination
     SendingDestination.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
+
